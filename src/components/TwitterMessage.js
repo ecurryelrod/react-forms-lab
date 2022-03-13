6f71a7b5acc5bda526e14f81671543dff5f9f7c3
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-13 15:10:31 -0600

    commit tree f21852eae5cb229ee57d320a64e52e631370d956

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {message: ''};
+  }
+
+  handleMessage = e => {
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
+    let characters = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+        onChange={this.handleMessage}
+        value={this.state.message}/>
+        <p>{characters}/280 characters remaining</p>
       </div>
     );
   }
